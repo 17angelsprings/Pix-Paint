570544d58891508b32025fa961fd3a0a9be43392
christy <cmiaogt@gmail.com>
2024-04-18 20:31:11 -0700

    implemented alternating square + possible grid for mapping

--- src/canvas-tmp/canvas.gd
+++ src/canvas-tmp/canvas.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+# canvas properties
+var canvas_size = Vector2(1000, 1000)  # size of canvas
+var pixel_size = 1  # size of pixel
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# canvas base
+func _draw():
+	# alternating squares
+	for y in range(int(canvas_size.y / pixel_size)):
+		for x in range(int(canvas_size.x / pixel_size)):
+			if (x + y) % 2 == 0:
+				draw_rect(Rect2(x * pixel_size, y * pixel_size, pixel_size, pixel_size), Color(0.9, 0.9, 0.9))
+			else:
+				draw_rect(Rect2(x * pixel_size, y * pixel_size, pixel_size, pixel_size), Color(0.8, 0.8, 0.8))
