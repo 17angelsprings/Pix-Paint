691a97566825565de8fd96ce7cd54010cff0d435
pomeloFellow <122317171+pomeloFellow@users.noreply.github.com>
2024-04-21 11:30:21 -0700

    Updated Spinbox to Set Value on Ready
    Added func get_global_var in canvas_globals to allow spinbox to grab value based on exported global_var

--- src/autoload/canvas_globals.gd
+++ src/autoload/canvas_globals.gd
@@ -3,6 +3,17 @@ extends Node
 var canvas_size = Vector2(100.0, 100.0)
 var current_layer_idx
 
+func get_global_variable(var_name):
+	match var_name:
+		"current_layer_idx":
+			return current_layer_idx
+		"canvas_size.x":
+			return canvas_size.x
+		"canvas_size.y":
+			return canvas_size.y
+		_:
+			print("Unknown global variable:", var_name)
+
 func set_global_variable(var_name, value):
 	print(value)
 	match var_name:
