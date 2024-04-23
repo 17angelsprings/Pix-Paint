85260603749dc17775a19fb5364a9d5b789482e7
GorgeousPanda <95512203+dasphelp@users.noreply.github.com>
2024-04-23 09:20:31 -0700

    Added global variable to keep track of whether the pen or eraser is active

--- src/autoload/tool_globals.gd
+++ src/autoload/tool_globals.gd
@@ -1,10 +1,29 @@
 extends Node
 
+# 0 is pen; 1 is eraser
+var pen_eraser: bool = 0
 var pen_size
 var pen_opacity
 var pen_color
 var eraser_size
 var eraser_opacity
+
+func get_global_variable(var_name):
+	match var_name:
+		"pen_eraser":
+			return pen_eraser
+		"pen_size":
+			return pen_size
+		"pen_opacity":
+			return pen_opacity
+		"pen_color":
+			return pen_color
+		"eraser_size":
+			return eraser_size
+		"eraser_opacity":
+			return eraser_opacity
+		_:
+			print("Unknown global variable:", var_name)
 
 func set_global_variable(var_name, value):
 	print(value)
