c60efe19d372c8b7372a0f30e6fc55c78546a8d0
17angelsprings <75920978+17angelsprings@users.noreply.github.com>
2024-04-26 14:23:19 -0700

    Opening Updates to Launcher

--- src/ui/menu/new_canvas.gd
+++ src/ui/menu/new_canvas.gd
@@ -1,17 +1,7 @@
 extends Control
 
-var project_name
-
-
 # OK button is pressed -> proceed to Workspace
 func _on_ok_pressed():
-	
-	# save project name
-	project_name = $MarginContainer/HBoxContainer/LineEdit.get_text()
-	if project_name == "":
-		project_name = "project"
-	ProjectGlobals.set_global_variable("project_name",project_name)
-	
 	get_tree().change_scene_to_file("res://src/workspace/workspace.tscn")
 
 # Cancel button is pressed -> go back to Main Menu
