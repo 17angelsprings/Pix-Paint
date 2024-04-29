c5367393286623de9cb63bcccade25a7e0966114
17angelsprings <75920978+17angelsprings@users.noreply.github.com>
2024-04-29 14:23:45 -0700

    Fixed New Button Functionality

--- src/ui/workspace/NewButton.gd
+++ src/ui/workspace/NewButton.gd
@@ -2,4 +2,5 @@ extends Node
 
 # Press this button to go back to the New Canvas menu and create a new canvas
 func _on_pressed():
+	FileGlobals.set_global_variable("image", Image.create(1000, 1000, false, Image.FORMAT_RGBA8))
 	get_tree().change_scene_to_file("res://src/ui/menu/new_canvas.tscn")
