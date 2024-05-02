27c70434c219f623a699fbed2c2c2aee70f429da
Christy Miao <cmiaogt@gmail.com>
2024-05-02 10:38:01 -0700

    add files archive

---
+++ src/canvas/canvas.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# canvas properties
+
+# size of pixel
+var pixel_size = 1
+
+# canvas base
+func _draw():
+	# alternating squares
+	for y in range(int(CanvasGlobals.canvas_size.y / pixel_size)):
+		for x in range(int(CanvasGlobals.canvas_size.x / pixel_size)):
+			if (x + y) % 2 == 0:
+				draw_rect(Rect2(x * pixel_size, y * pixel_size, pixel_size, pixel_size), Color(0.9, 0.9, 0.9))
+			else:
+				draw_rect(Rect2(x * pixel_size, y * pixel_size, pixel_size, pixel_size), Color(0.8, 0.8, 0.8))
