b594278a3ef11b30bff0dfbcc8e48b4edad63cce
pomeloFellow <122317171+pomeloFellow@users.noreply.github.com>
2024-05-09 11:01:53 -0700

    Modify Canvas Viewport To Enable Zoom
    Still need to connect zoom control to mouse.
    
    But can see zoom functionality by changing zoom var in camera2d editor.

--- src/workspace/canvas_viewport.gd
+++ src/workspace/canvas_viewport.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var subviewcontainer: SubViewportContainer
 @export var subviewport: SubViewport
+@export var camerasubviewcontainer: SubViewportContainer
+@export var camerasubviewport: SubViewport
 
 
 func _ready():
@@ -18,21 +20,31 @@ func _on_canvas_size_changed():
 func update_canvas_size():
 	# set viewport size to match canvas
 	subviewport.size_2d_override = CanvasGlobals.canvas_size
+	camerasubviewport.size_2d_override = CanvasGlobals.canvas_size
 	
 	# set container size
 	if CanvasGlobals.canvas_size.x == CanvasGlobals.canvas_size.y:
 		subviewcontainer.size.x = 500
 		subviewcontainer.size.y = 500
+		camerasubviewcontainer.size.x = 500
+		camerasubviewcontainer.size.y = 500
 	# if width is bigger, x should be 600 and y should scale according to canvas size
 	elif CanvasGlobals.canvas_size.x > CanvasGlobals.canvas_size.y:
 		subviewcontainer.size.x = 500
 		subviewcontainer.size.y = (500 * CanvasGlobals.canvas_size.y)/CanvasGlobals.canvas_size.x
+		camerasubviewcontainer.size.x = 500
+		camerasubviewcontainer.size.y = (500 * CanvasGlobals.canvas_size.y)/CanvasGlobals.canvas_size.x
 	# if length is bigger, y should be 600 and x should scale according to canvas size
 	else:
 		subviewcontainer.size.y = 500
 		subviewcontainer.size.x = (500 * CanvasGlobals.canvas_size.x)/CanvasGlobals.canvas_size.y
+		camerasubviewcontainer.size.y = 500
+		camerasubviewcontainer.size.x = (500 * CanvasGlobals.canvas_size.x)/CanvasGlobals.canvas_size.y
+		
 	# set container position, so that the canvas stays centered
 	subviewcontainer.position.x = -(subviewcontainer.size.x/2)
 	subviewcontainer.position.y = -(subviewcontainer.size.y/2)
+	camerasubviewcontainer.position.x = -(subviewcontainer.size.x/2)
+	camerasubviewcontainer.position.y = -(subviewcontainer.size.y/2)
 	print("SubViewport Size: ", subviewport.size_2d_override)
 	print("SubViewContainer Size: ", subviewcontainer.size)
