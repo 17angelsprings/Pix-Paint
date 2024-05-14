913890172be0197af5ef4a2e89341180564bc0a6
17angelsprings <75920978+17angelsprings@users.noreply.github.com>
2024-05-13 18:56:24 -0700

    Resize Bug Fix

--- src/ui/menu/new_canvas.gd
+++ src/ui/menu/new_canvas.gd
@@ -6,6 +6,8 @@ func _on_ok_pressed():
 	if project_name == "":
 		project_name = "project"
 	FileGlobals.set_global_variable("project_name",project_name)	
+	var image = Image.create(CanvasGlobals.get_global_variable("canvas_size.x"), CanvasGlobals.get_global_variable("canvas_size.y"), false, Image.FORMAT_RGBA8)
+	FileGlobals.set_global_variable("image", image)
 	get_tree().change_scene_to_file("res://src/workspace/workspace.tscn")
 
 # Cancel button is pressed -> go back to Main Menu
