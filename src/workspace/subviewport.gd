87b5833c50d9c584e9f083d0bbcd7458bb8fa5dc
pomeloFellow <122317171+pomeloFellow@users.noreply.github.com>
2024-05-17 18:01:53 -0700

    Zoom Connected to Mouse
    Mouse scroll zooms, but only into center.
    Still need to add so based on mouse position.

---
+++ src/workspace/subviewport.gd
@@ -0,0 +1,13 @@
+extends SubViewport
+
+@export var canvas_camera: Camera2D
+var mouse_pos
+
+## Used to detect mouse interaction for mouse
+func _input(event):	
+	if event is InputEventMouseMotion:
+		mouse_pos = event.position
+	elif event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+		canvas_camera.zoom_io(canvas_camera.change_in_zoom, canvas_camera.offset)
+	elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+		canvas_camera.zoom_io(-canvas_camera.change_in_zoom, canvas_camera.offset)
