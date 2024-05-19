fc92bba7ad14f5d0d599c6cb1716643ce2203a26
pomeloFellow <122317171+pomeloFellow@users.noreply.github.com>
2024-05-19 11:26:40 -0700

    Connect mouse position to zoom

--- src/workspace/subviewportcontainer.gd
+++ src/workspace/subviewportcontainer.gd
@@ -3,20 +3,25 @@ extends SubViewportContainer
 @export var canvas_camera: Camera2D
 var mouse_pos
 
-## Used to detect mouse interaction for mouse
+## Used to detect mouse interaction for canvas camera
 func _input(event):	
 	if event is InputEventMouseMotion:
 		mouse_pos = event.position
 
 	elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:		# zoom in
-		# print(normal(mouse_pos))
 		canvas_camera.zoom_io(canvas_camera.change_in_zoom, canvas_camera.offset)
+		# offset to keep mouse in same place
+		var zoom_term = (canvas_camera.zoom * canvas_camera.zoom)
+		canvas_camera.offset += (Vector2(26,26) / zoom_term) * normal(mouse_pos)
 
 	elif event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:			# zoom out
-		#print(normal(mouse_pos))
 		canvas_camera.zoom_io(-canvas_camera.change_in_zoom, canvas_camera.offset)
-
+		# offset to keep mouse in same place
+		var zoom_term = ((canvas_camera.zoom + Vector2(0.1,0.1)) * (canvas_camera.zoom + Vector2(0.1,0.1)))
+		canvas_camera.offset -= (Vector2(26,26) / zoom_term) * normal(mouse_pos)
+		
 	elif event.button_index == MOUSE_BUTTON_MIDDLE and event.pressed:
+		#print(normal(mouse_pos))
 		print(canvas_camera.offset)
 
 ## normalize vector (0,500) to (-1,1)
