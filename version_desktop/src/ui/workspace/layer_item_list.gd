9f133a76c0b6c9f6c57914bf80b89808f1414376
pomeloFellow <122317171+pomeloFellow@users.noreply.github.com>
2024-05-28 15:00:15 -0700

    Draws On Separate Layers

--- version_desktop/src/ui/workspace/layer_item_list.gd
+++ version_desktop/src/ui/workspace/layer_item_list.gd
@@ -12,27 +12,33 @@ func _ready():
 func _on_item_selected(index):
 	# set current layer
 	list_idx = index
-	CanvasGlobals.current_layer_idx = index
+	var lm_idx = (item_count - list_idx - 1)
+	CanvasGlobals.current_layer_idx = lm_idx
+	layer_manager.change_layer_to(lm_idx)
 	
-	# print(CanvasGlobals.current_layer_idx)
+	print(CanvasGlobals.current_layer_idx)
 
 
 func _on_add_layer_button_pressed():
 	# add item above currently selected layer
+	# set num layers
 	var layer_num = CanvasGlobals.get_global_variable("num_layers")
 	layer_num += 1
 	CanvasGlobals.set_global_variable("num_layers", layer_num)
 	
+	# add item to list
 	var last_idx = add_item("Layer " + str(layer_num), null, true)
 	move_item(last_idx, list_idx)
 	select(list_idx, true)
-	# print(CanvasGlobals.current_layer_idx)
-	var lm_idx = (item_count - list_idx - 1)
-	print("LM Children Idx: ", lm_idx)
-	layer_manager.add_layer_at(lm_idx)
 	
 	# add new layer
+	var lm_idx = (item_count - list_idx - 1)
+	layer_manager.add_layer_at(lm_idx)
 	
+	# set curr layer
+	CanvasGlobals.current_layer_idx = lm_idx
+	layer_manager.change_layer_to(lm_idx)
+	print(CanvasGlobals.current_layer_idx)
 
 
 func _on_delete_layer_button_pressed():
@@ -40,10 +46,17 @@ func _on_delete_layer_button_pressed():
 		# if only one layer left, don't remove
 		return
 	else:
-		
-		# delete layer
-		
 		# update indices
+		remove_item(list_idx)
 		if list_idx == item_count:
 			list_idx -= 1
 		select(list_idx, true)
+		
+		# delete layer
+		var lm_idx = (item_count - list_idx - 1)
+		layer_manager.delete_layer_at(lm_idx)
+		
+		# set curr layer
+		CanvasGlobals.current_layer_idx = lm_idx
+		layer_manager.change_layer_to(lm_idx)
+		print(CanvasGlobals.current_layer_idx)
