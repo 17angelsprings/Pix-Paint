94efe143d7e6350c98fd975f072e1b52be08c9af
17angelsprings <75920978+17angelsprings@users.noreply.github.com>
2024-06-03 17:37:19 -0700

    Update BackButton.gd documentation

--- version_desktop/src/ui/menu/BackButton.gd
+++ version_desktop/src/ui/menu/BackButton.gd
@@ -1,8 +1,32 @@
+## BACK BUTTON.GD
+## ********************************************************************************
+## Script for handling button presses that quit or terminate the application.
+## ********************************************************************************
+
+## ASSOCIATED SCENES
+## ********************************************************************************
+## creds_perms.tcsn
+## help.tcsn
+## ********************************************************************************
+
+## EXTENSIONS
+## ********************************************************************************
 extends Button
+## ********************************************************************************
 
+## SCRIPT-WIDE VARIABLES
+## ********************************************************************************
 
+## Path to main menu scene
 var menu_scene = "res://src/ui/menu/menu.tscn"
 
+
+## FUNCTIONS
+## ********************************************************************************
+
+## Changes scene back to main menu when pressed
+## @params: none
+## @return: none
 func _on_pressed():
 	## Navigate to the Main Menu scene
 	get_tree().change_scene_to_file(menu_scene)
