f38b41e1af661e47a12c38acf257c979a0647b91
17angelsprings <75920978+17angelsprings@users.noreply.github.com>
2024-06-03 18:50:11 -0700

    Update NewButton.gd documentation

--- version_desktop/src/ui/workspace/NewButton.gd
+++ version_desktop/src/ui/workspace/NewButton.gd
@@ -1,6 +1,30 @@
-extends Node
+# NEW BUTTON .GD
+## ********************************************************************************
+## Script that handles interaction and functionality of the New button
+## ********************************************************************************
 
-# Press this button to go back to the New Canvas menu and create a new canvas
+## ASSOCIATED SCENES
+## ********************************************************************************
+## workspace_ui.tcsn
+## ********************************************************************************
+
+## EXTENSIONS
+## ********************************************************************************
+extends Node
+## ********************************************************************************
+
+## SCRIPT-WIDE VARIABLES
+## ********************************************************************************
+## none
+## ********************************************************************************
+
+## FUNCTIONS
+## ********************************************************************************
+
+## New button has been pressed so the scene changes
+## to the New Canvas scene
+## @params: none
+## @return: none
 func _on_pressed():
 	CanvasGlobals.set_global_variable("image", Image.create(CanvasGlobals.get_global_variable("canvas_size.x"), CanvasGlobals.get_global_variable("canvas_size.y"), false, Image.FORMAT_RGBA8))
 	get_tree().change_scene_to_file("res://src/ui/menu/new_canvas.tscn")
